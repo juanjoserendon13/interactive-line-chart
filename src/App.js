d52f0002f7c8fce93032f349865a26ac197b85b6
Juan Jose Rendon <juanjoserendon13@outlook.com>
2019-06-13 15:19:39 -0500

    base component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
+
+const LineChart = lazy(() => import('./components/LineChart'));
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Suspense fallback={<div>loading...</div>}>
+        <LineChart />
+      </Suspense>
+    </div>
   );
 }
 
